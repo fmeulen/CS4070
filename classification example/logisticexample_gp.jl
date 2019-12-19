2825b6cf301b57bf25cc410ea9068972dfc87404
fmeulen <f.h.vandermeulen@tudelft.nl>
2019-12-19 11:44:14 +0100

    Update logisticexample_gp.jl

--- classification example/logisticexample_gp.jl
+++ classification example/logisticexample_gp.jl
@@ -20,7 +20,7 @@ p = size(X)[2]
 scatter(X[:,1], X[:,2])
 
 # to experiment, we may optionally add some extra data to complicated the classification task
-addextrapoints = true#false#true
+addextrapoints = false
 if addextrapoints # add some red points to the topright corner
       Xextra = [4.0 5.0; 4.1 5.1; 3.9 3.9; 5.0 5.0; 5.1 -0.1]
       yextra = zeros(5)
@@ -29,7 +29,7 @@ if addextrapoints # add some red points to the topright corner
 end
 
 # choose a kernel for the gp
-kernchoice = [:matern, :se][2]
+kernchoice = [:matern, :se][1]
 
 # fit gaussian process
 mZero = MeanZero()
